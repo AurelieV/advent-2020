b508e55c58b44aa7eee412f619fc3163eccdd486
Aurélie VIOLETTE <orwel.violette@gmail.com>
2021-02-08 12:01:00 +0100

    Jour 20 - WIP Exo 2

--- jour20/index.js
+++ jour20/index.js
@@ -36,11 +36,43 @@ function canVersionsMatch(versionA, versionB) {
     );
 }
 
+function getDirectionsMatch(versionA, versionB) {
+    if (isBorderMatching(versionA.top, versionB.bottom)) {
+        return ["top", "bottom"];
+    } else if (isBorderMatching(versionA.bottom, versionB.top)) {
+        return ["bottom", "top"];
+    } else if (isBorderMatching(versionA.right, versionB.left)) {
+        return ["right", "left"];
+    } else if (isBorderMatching(versionA.left, versionB.right)) {
+        return ["left", "right"];
+    }
+
+    return null;
+}
+
 function canTilesMatch(tileA, tileB) {
     const versionsA = tileA.versions;
     const versionsB = tileB.versions;
 
     return versionsA.some((versionA) => versionsB.some((versionB) => canVersionsMatch(versionA, versionB)));
+}
+
+function getTilesMatches(tileA, tileB) {
+    const matches = [];
+    tileA.versions.forEach((versionA, indexVersionA) => {
+        tileB.versions.forEach((versionB, indexVersionB) => {
+            const directions = getDirectionsMatch(versionA, versionB);
+            if (directions) {
+                matches.push({
+                    direction: directions[0],
+                    version: indexVersionA,
+                    with: { id: tileB.id, version: indexVersionB },
+                });
+            }
+        });
+    });
+
+    return matches;
 }
 
 function addMatch(matches, idA, idB) {
@@ -60,6 +92,78 @@ function printTile(tile) {
         }
     });
     console.log(tile.bottom.join(""));
+}
+
+function getNext(previous, matches, direction) {
+    const match = matches
+        .get(previous.id)
+        .find((match) => match.direction === direction && match.version === previous.version);
+
+    return match.with;
+}
+
+function getImage(partialImage, size, matches) {
+    if (partialImage.length === size && partialImage[size - 1].length === size) return partialImage;
+    let y = partialImage.length - 1;
+    let x = partialImage[y].length;
+    if (x === size) {
+        y++;
+        x = 0;
+        partialImage.push([]);
+    }
+    if (x === 0) {
+        partialImage[y].push(getNext(partialImage[y - 1][0], matches, "bottom"));
+    } else {
+        partialImage[y].push(getNext(partialImage[y][x - 1], matches, "right"));
+    }
+    return getImage(partialImage, size, matches);
+}
+
+function cutFirstAndLast(arr) {
+    return arr.slice(1, -1);
+}
+
+function rotate(rows) {
+    const result = [];
+    for (let i = 0; i < rows[0].length; i++) {
+        const newRow = rows.map((row) => row[i]).reverse();
+        result.push(newRow);
+    }
+
+    return result;
+}
+
+function getVersion(rows, version) {
+    if (version >= 4) {
+        rows = rows.map((_, index) => rows[rows.length - 1 - index]);
+    }
+    const nbRotations = version % 4;
+    for (let i = 1; i <= nbRotations; i++) {
+        rows = rotate(rows);
+    }
+
+    return rows;
+}
+
+function convertTile(tileById, { id, version }) {
+    let rows = tileById.get(id).rows;
+    rows = cutFirstAndLast(rows.map(cutFirstAndLast));
+
+    return getVersion(rows, version);
+}
+function convertLine(line, tileById) {
+    const rows = [];
+    const tiles = line.map((tile) => convertTile(tileById, tile));
+    for (let i = 0; i < tiles[0].length; i++) {
+        const row = tiles.reduce((all, tile) => all.concat(tile[i]), []);
+        rows.push(row);
+    }
+
+    return rows;
+}
+
+function convertImage(image, tileById) {
+    return image.reduce((result, line) => result.concat(convertLine(line, tileById)), []);
 }
 
 function main() {
@@ -94,26 +198,40 @@ function main() {
         return {
             id,
             versions,
+            rows,
         };
     });
 
     const matches = new Map(tiles.map((tile) => [tile.id, []]));
     const nbTiles = tiles.length;
     for (let indexA = 0; indexA <= nbTiles - 1; indexA++) {
-        for (let indexB = indexA + 1; indexB <= nbTiles - 1; indexB++) {
-            const isMatching = canTilesMatch(tiles[indexA], tiles[indexB]);
-            if (isMatching) {
-                addMatch(matches, tiles[indexA].id, tiles[indexB].id);
+        for (let indexB = 0; indexB <= nbTiles - 1; indexB++) {
+            if (indexA !== indexB) {
+                const tileMatches = getTilesMatches(tiles[indexA], tiles[indexB]);
+                if (tileMatches.length) {
+                    matches.set(tiles[indexA].id, matches.get(tiles[indexA].id).concat(tileMatches));
+                }
             }
         }
     }
 
     const corners = [];
-    matches.forEach((ids, id) => {
-        if (ids.length === 2) {
+    matches.forEach((matchWith, id) => {
+        const ids = matchWith.map((match) => match.with.id);
+        const matchingTiles = new Set(ids);
+        if (matchingTiles.size === 2) {
             corners.push(id);
         }
     });
+
+    const image = getImage([[{ id: corners[0], version: 0 }]], Math.sqrt(nbTiles), matches);
+    const tileById = new Map(
+        tiles.map((tile) => {
+            return [tile.id, tile];
+        })
+    );
+    const final = convertImage(image, tileById);
+    console.log(final);
 
     const result = corners.reduce((p, corner) => p * corner, 1);
 
